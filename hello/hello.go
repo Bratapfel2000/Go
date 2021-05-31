ac80d6073e5708e846abe0221a6ef6ed622d0b52
bratapfel <bratapfel2k@gmail.com>
2021-05-31 14:37:02 +0200

    refactoring hello_test and fix hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 // adding constants
+// fix error with if
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+    if name == "" {
+        name = "World"
+    }
     return englishHelloPrefix + name
 }
 
